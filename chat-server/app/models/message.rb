8211bdae0a8921853edd1a619218ea1e1372801e
YuriBocharov <quesadillaman@gmail.com>
2023-11-29 11:48:17 -0500

    styling

--- chat-server/app/models/message.rb
+++ chat-server/app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
   after_create_commit do
-    broadcast_append_to(:room, chat_room, target: "messages", partial: "messages/message", locals: {message: self})
+    broadcast_append_to(:room, chat_room, target: "messages", partial: "messages/message", locals: {message: self, user: current_user})
   end
 end
